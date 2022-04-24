5a55e3f1ca0a06f823dc36e04df36e0d86a797fb
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-24 08:53:15 -0700

    Add notes on javascript data types

--- JavaScript-lessons/javascript.js
+++ JavaScript-lessons/javascript.js
@@ -1,2 +1,6 @@
-let x = 1;
-console.log(++x);
+let str = 'hello my name is alex.';
+
+console.log(str.length);
+console.log(str.slice(0, 4));
+console.log(str.substring(0, 4));
+console.log(str.substr(0, 4));
