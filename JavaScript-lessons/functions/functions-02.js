843aca67e3fb30e516f7fe5215411dbeedf7f012
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-26 14:39:48 -0700

    Do examples for javascript foundations 3

---
+++ JavaScript-lessons/functions/functions-02.js
@@ -0,0 +1,23 @@
+function addSeven(number) {
+  return number + 7;
+}
+
+console.log(addSeven(1));
+
+function multiply(numOne, numTwo) {
+  return numOne * numTwo;
+}
+
+console.log(multiply(2, 3));
+
+function capitalize(str) {
+  return str.toUpperCase().slice(0, 1) + str.slice(1, str.length + 1).toLowerCase();
+}
+
+console.log(capitalize('aPPLE'));
+
+function getLastLetter(str) {
+  return str.slice(-1);
+}
+
+console.log('apple');
