23df25f9b72470ab6b34d6005680cb3238c8ce91
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-30 08:37:33 -0700

    Add solution to sumAll

--- JavaScript-lessons/javascript-exercises/05_sumAll/sumAll.js
+++ JavaScript-lessons/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (Array.from(arguments).length > 2) {
+    return 'ERROR';
+  } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return 'ERROR';
+  } else if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
 
+  let big = num1 < num2 ? num2 : num1;
+  let small = num1 > num2 ? num2 : num1;
+
+  let sum = 0;
+
+  for (let i = small; i <= big; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
