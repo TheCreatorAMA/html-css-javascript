1b44b4805f86dc834587ebe6a7eb1e59353916ba
TheCreatorAMA <alexanderakiu@gmail.com>
2022-04-30 08:26:30 -0700

    Add solution to removeFromArray. FYI -1 is not falsy

--- JavaScript-lessons/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ JavaScript-lessons/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let inputs = Array.from(arguments);
+  let array = inputs.splice(0, 1)[0];
 
+  for (let item of inputs) {
+    if (array.indexOf(item) !== -1) {
+      array.splice(array.indexOf(item), 1);
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
