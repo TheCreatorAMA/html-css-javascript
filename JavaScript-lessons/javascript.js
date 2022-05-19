bc5d3dff8f8484f8177e7df1178d20b5bf9ee64b
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-19 07:22:48 -0700

    Add notes on objects

--- JavaScript-lessons/javascript.js
+++ JavaScript-lessons/javascript.js
@@ -1,6 +1,1 @@
-let str = 'hello my name is alex.';
-
-console.log(str.length);
-console.log(str.slice(0, 4));
-console.log(str.substring(0, 4));
-console.log(str.substr(0, 4));
+const notify = new Notification('Hello!');
