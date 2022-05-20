7ca5299c66ac1407c4d8f6fc5d80ebef90f72376
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-20 08:08:51 -0700

    Add solution to palindrome exercise

--- JavaScript-lessons/javascript-exercises/09_palindromes/palindromes.js
+++ JavaScript-lessons/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanedStr = str.replace(/[^a-z]/gi, '').toLowerCase();
+  let reverse = cleanedStr.split('').reverse().join('');
 
+  return cleanedStr === reverse;
 };
 
 // Do not edit below this line
