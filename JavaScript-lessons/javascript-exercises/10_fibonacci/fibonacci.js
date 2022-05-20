b1631a66db72244837e2a91e8e96505e362c4b41
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-20 08:22:52 -0700

    Add solution to Fibonacci exercise

--- JavaScript-lessons/javascript-exercises/10_fibonacci/fibonacci.js
+++ JavaScript-lessons/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num, memo = { 0: 0, 1: 1, 2: 1 }) {
+  if (num < 0) {
+    return 'OOPS';
+  }
 
+  if (memo[num]) {
+    return memo[num];
+  }
+
+  memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+
+  return memo[num];
 };
 
 // Do not edit below this line
