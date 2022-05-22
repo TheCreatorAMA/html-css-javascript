0d6f26f78f5e25f9991df68305200acaef0037f4
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-22 09:41:24 -0700

    Add solution to findTheOldest javascript exercise

--- JavaScript-lessons/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ JavaScript-lessons/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function () {
+  let people = arguments[0];
 
+  return people.reduce((prevPerson, curPerson) => {
+    let currentYear = new Date().getFullYear();
+
+    let prevPersonAge;
+    let curPersonAge;
+
+    if (prevPerson === 0) {
+      prevPersonAge = 0;
+    } else if (prevPerson.yearOfDeath) {
+      prevPersonAge = prevPerson.yearOfDeath - prevPerson.yearOfBirth;
+    } else {
+      prevPersonAge = currentYear - prevPerson.yearOfBirth;
+    }
+
+    if (curPerson.yearOfDeath) {
+      curPersonAge = curPerson.yearOfDeath - curPerson.yearOfBirth;
+    } else {
+      curPersonAge = currentYear - curPerson.yearOfBirth;
+    }
+
+    return prevPersonAge > curPersonAge ? prevPerson : curPerson;
+  }, 0);
 };
 
 // Do not edit below this line
